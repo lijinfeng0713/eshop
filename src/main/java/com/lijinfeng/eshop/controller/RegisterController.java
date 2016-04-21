e048a2fc24f748929fd4793263adf1c60dc76cf5
lijinfeng <1063810293@qq.com>
2016-04-21 11:53:32 +0800

    调整用户注册检查策略

--- src/main/java/com/lijinfeng/eshop/controller/RegisterController.java
+++ src/main/java/com/lijinfeng/eshop/controller/RegisterController.java
@@ -2,12 +2,13 @@ package com.lijinfeng.eshop.controller;
 
 import com.lijinfeng.eshop.entity.User;
 import com.lijinfeng.eshop.service.UserService;
+import net.sf.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Created by ljf-梁燕双栖 on 2016/4/20.
@@ -26,26 +27,13 @@ public class RegisterController {
     @RequestMapping(value = "/register", method = RequestMethod.POST)
     public @ResponseBody
     String register (@RequestBody User u) {
-        System.out.println(u);
+        User user = userService.findByUsername(u.getUsername());
+        if (user != null || "".equals(u.getUsername()) || "".equals(u.getPassword()) ||"".equals(u.getEmail()))
+            return "{ \"SUCCESS\" : false }";
         try {
             userService.addUser(u);
             return "{ \"SUCCESS\" : true }";
         } catch (Exception ex) {
-            return "{ \"SUCCESS\" : false }";
-        }
-    }
-
-    /*
-	 * 功能：检测用户名是否已经被注册
-	 */
-    @RequestMapping(value = "/check", method = RequestMethod.POST)
-    @ResponseBody
-    public String registerCheck(@RequestBody User user) {
-
-        User u = userService.findByUsername(user.getUsername());
-        if(u == null){
-            return "{ \"SUCCESS\" : true }";
-        }else{
             return "{ \"SUCCESS\" : false }";
         }
     }
